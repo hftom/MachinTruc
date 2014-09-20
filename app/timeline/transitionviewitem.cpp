823a10d3f04960bfe04819f9106c602cd509e108
Christophe Thommeret <hftom@free.fr>
2014-09-20 19:24:29 +0200

    Make transition item more tranluscent.

--- app/timeline/transitionviewitem.cpp
+++ app/timeline/transitionviewitem.cpp
@@ -21,8 +21,8 @@ TransitionViewItem::TransitionViewItem( QGraphicsItem *parent, double pos, doubl
 
 	QLinearGradient grad( QPointF(0, 0), QPointF(0, 1) );
 	grad.setCoordinateMode( QGradient::ObjectBoundingMode );
-	grad.setColorAt( 0, QColor(200,255,0,180) );
-	grad.setColorAt( 1, QColor(128,178,0,180) );
+	grad.setColorAt( 0, QColor(200,255,0,100) );
+	grad.setColorAt( 1, QColor(128,178,0,100) );
 	normalBrush = QBrush( grad );
 	
 	setPen( normalPen );
