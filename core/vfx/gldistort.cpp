2cc89f809e96e387be8c21de0f25f67dbca8d009
Christophe Thommeret <hftom@free.fr>
2017-06-10 23:43:01 +0200

    fix gldistort

--- core/vfx/gldistort.cpp
+++ core/vfx/gldistort.cpp
@@ -1,3 +1,5 @@
+#include <movit/resample_effect.h>
+#include <movit/padding_effect.h>
 #include "vfx/gldistort.h"
 
 
@@ -15,27 +17,31 @@ GLDistort::~GLDistort()
 
 
 
-void GLDistort::preProcess( Frame *src, Profile *p )
-{
-	src->glSAR *=  getParamValue( ratio ).toFloat();
-}
-
-
-
-QString GLDistort::getDescriptor( double pts, Frame *src, Profile *p )
-{
-	preProcess( src, p );
-	return getIdentifier();
-}
-
-
-
 bool GLDistort::process( const QList<Effect*> &el, double pts, Frame *src, Profile *p )
 {
-	preProcess( src, p );
-
-	if ( src->glOVD )
-		src->glOVDTransformList.append( FilterTransform( FilterTransform::SCALE, (double)src->glWidth, (double)src->glHeight * src->glSAR ) );
+	int w, h;
+	float top = 0.0f;
+	float left = 0.0f;
+	
+	double sar = getParamValue( ratio ).toFloat();
+	
+	if (sar < 1) {
+		w = src->glWidth * sar;
+		h = src->glHeight;
+		left = (float)(src->glWidth - w) / 2.0f;
+	}
+	else {
+		w = src->glWidth;
+		h = src->glHeight / sar;
+		top = (float)(src->glHeight - h) / 2.0f;
+	}
+	
+	return el[0]->set_int( "width", w )
+		&& el[0]->set_int( "height", h )
+		&& el[1]->set_int("width", src->glWidth)
+		&& el[1]->set_int("height", src->glHeight)
+		&& el[1]->set_float("top", top)
+		&& el[1]->set_float("left", left);
 	
 	return true;
 }
@@ -45,5 +51,7 @@ bool GLDistort::process( const QList<Effect*> &el, double pts, Frame *src, Profi
 QList<Effect*> GLDistort::getMovitEffects()
 {
 	QList<Effect*> list;
+	list.append( new ResampleEffect() );
+	list.append( new PaddingEffect() );
 	return list;
 }
--- core/vfx/gldistort.h
+++ core/vfx/gldistort.h
@@ -10,15 +10,12 @@ class GLDistort : public GLFilter
 public:
 	GLDistort( QString id, QString name );
 	~GLDistort();
-	
-	QString getDescriptor( double pts, Frame *src, Profile *p );
+
 	bool process( const QList<Effect*> &el, double pts, Frame *src, Profile *p );
 
 	QList<Effect*> getMovitEffects();
 	
 private:
-	void preProcess( Frame *src, Profile *p );
-	
 	Parameter *ratio;
 };
 
